598dbdc84e63e5c69ba0716017b579e9adf826e9
Sushma Ramanujam <rsushmagautam@gmail.com>
2025-03-09 04:30:27 -0400

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export", // Enables static export
+  basePath: "/soltam.github.io", // Replace <repo-name> with your GitHub repository name
+  assetPrefix: "/soltam.github.io/",
   images: {
+    unoptimized: true, // Required to serve images without Next.js optimizations,
     remotePatterns: [
       {
         protocol: "https",
